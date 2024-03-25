488975e4dc3662a782c6be68d21a31d2b24043fc
sakibhossain323 <sakibhossain323@gmail.com>
2024-03-26 00:34:31 +0600

    reduced geo formula

--- notebook/Codes/[00]Data Structure/[09]SegmentTree.cpp
+++ notebook/Codes/[00]Data Structure/[09]SegmentTree.cpp
@@ -96,15 +96,3 @@ void solve(){
   }
   return;
 }
-
-int main(){
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
-
-  int tt=1; //cin>>tt;
-  while(tt--){
-    solve();
-  }
-  return 0;
-}
-
